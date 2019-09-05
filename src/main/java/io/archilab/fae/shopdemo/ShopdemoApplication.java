f4bbaf97f6935ae69ad96c4c7e7ec59bd5072c73
JannIntveen <JannIntveen@gmail.com>
2019-09-05 10:43:01 +0200

    Added initial project

---
+++ src/main/java/io/archilab/fae/shopdemo/ShopdemoApplication.java
@@ -0,0 +1,13 @@
+package io.archilab.fae.shopdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShopdemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/archilab/fae/shopdemo/ShopdemoApplicationTests.java
@@ -0,0 +1,16 @@
+package io.archilab.fae.shopdemo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ShopdemoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
