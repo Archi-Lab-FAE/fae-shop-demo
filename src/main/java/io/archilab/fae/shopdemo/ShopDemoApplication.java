52970e54ca35d036059e9a10dacdb0bfb9698f2d
JannIntveen <JannIntveen@gmail.com>
2019-09-05 11:46:08 +0200

    Added Docker support

--- src/main/java/io/archilab/fae/shopdemo/ShopDemoApplication.java
+++ src/main/java/io/archilab/fae/shopdemo/ShopDemoApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ShopdemoApplication {
+public class ShopDemoApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ShopdemoApplication.class, args);
+		SpringApplication.run(ShopDemoApplication.class, args);
 	}
 
 }
--- src/test/java/io/archilab/fae/shopdemo/ShopDemoApplicationTests.java
+++ src/test/java/io/archilab/fae/shopdemo/ShopDemoApplicationTests.java
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class ShopdemoApplicationTests {
+public class ShopDemoApplicationTests {
 
 	@Test
 	public void contextLoads() {
