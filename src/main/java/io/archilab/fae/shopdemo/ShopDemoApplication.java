df62e3d2cd66621369e74d9192592d4bfbdbbeef
JannIntveen <JannIntveen@gmail.com>
2019-09-12 15:26:12 +0200

    Added forwarded header filter

--- src/main/java/io/archilab/fae/shopdemo/ShopDemoApplication.java
+++ src/main/java/io/archilab/fae/shopdemo/ShopDemoApplication.java
@@ -2,11 +2,18 @@ package io.archilab.fae.shopdemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.filter.ForwardedHeaderFilter;
 
 @SpringBootApplication
 public class ShopDemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ShopDemoApplication.class, args);
+	}
+
+	@Bean
+	ForwardedHeaderFilter forwardedHeaderFilter() {
+		return new ForwardedHeaderFilter();
 	}
 }
